4203f4532bfe77be31e5ebb6bf4ad0e5dba4aad0
Dominic Perini <dominic.perini@gmail.com>
2015-12-03 15:39:39 +0000

    tidied up

--- Prova_test.hs
+++ Prova_test.hs
@@ -1,24 +1,21 @@
 
 
-  module Exercise 
+  module Prova_test
     where
-    
-  import Prelude
-  import Data.Char
-  import Data.List
+
   import Debug.Trace
   
   
   
   
-  
+  trace_same :: Show a => String -> a -> a
   trace_same message x = trace (message ++ (show x)) x
   
  
   cube :: Int -> Int
   cube x = trace_same (show ("cube_fun: " ++ "cube_input: " ++ show (x) ++ " cube_output: ")) (x^3)
   
-  conbine :: Int -> Int -> Int
-  conbine x y = ((cube x) + (cube y))
+  combine :: Int -> Int -> Int
+  combine x y = ((cube x) + (cube y))
   
  
