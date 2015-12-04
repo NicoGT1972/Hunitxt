c0c23eb3e5b580cef0d1c4983baf4e2422a05405
Dominic Perini <dominic.perini@gmail.com>
2015-12-04 10:44:36 +0000

    proposal for uncurry

--- Prova_test.hs
+++ Prova_test.hs
@@ -14,13 +14,19 @@
   trace_capture :: Show a => [a] -> a -> a
   trace_capture parameters result =
     trace ("(" ++ show (length parameters) ++ "," ++ show parameters ++ "," ++ show result ++ "),") result
- 
+
+  unc :: (Eq a, Num a) => a -> [b] -> (b -> c)-> c
+  unc arity list function
+    | arity == 1 = unc1 list function
+
+  unc1 [a] f = f a
+
   cube :: Int -> Int
   cube x =
 --    trace_same ("input:" ++ show x ++ " output:") $
     trace_capture [x] $
     x^(3::Int)
-  
+
   combine :: Int -> Int -> Int
   combine x y = (cube x) + (cube y)
   
