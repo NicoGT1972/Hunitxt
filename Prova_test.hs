b690e5738989f78a986f97d9153b58fd4fb134fa
Dominic Perini <dominic.perini@gmail.com>
2015-12-04 10:01:06 +0000

    trace_capture function added

--- Prova_test.hs
+++ Prova_test.hs
@@ -9,15 +9,20 @@
   
   
   trace_same :: Show a => String -> a -> a
-  trace_same message x = trace (message ++ (show x)) x
+  trace_same message x = trace (message ++ show x) x
+
+  trace_capture :: Show a => a -> a -> a
+  trace_capture parameters result =
+    trace ("(" ++ show parameters ++ "," ++ show result ++ "),") result
   
  
   cube :: Int -> Int
   cube x =
-    trace_same ("input:" ++ show x ++ " output:") $
-    (x^(3::Int))
+--    trace_same ("input:" ++ show x ++ " output:") $
+    trace_capture x $
+    x^(3::Int)
   
   combine :: Int -> Int -> Int
-  combine x y = ((cube x) + (cube y))
+  combine x y = (cube x) + (cube y)
   
  
